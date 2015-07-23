5b4e32227fd8726b3c56c24da5ddc182f77a48ad
maeglin89273 <maeglin89273@gmail.com>
2015-07-23 12:24:20 +0800

    #Make InteractivePlotPlugin extend PlotPlugin

--- src/view/component/plugin/InteractivePlotPlugin.java
+++ src/view/component/plugin/InteractivePlotPlugin.java
@@ -3,6 +3,6 @@ package view.component.plugin;
 /**
  * Created by maeglin89273 on 7/23/15.
  */
-public interface InteractivePlotPlugin {
-    public void registerListeners();
+public abstract class InteractivePlotPlugin extends PlotPlugin {
+    public abstract void registerListeners();
 }
